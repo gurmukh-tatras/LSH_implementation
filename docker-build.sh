14876790457934301bde4ec7ba46c3f69b186cd2
gurmukh <gurmukh@tatrasdata.com>
2020-03-19 18:58:41 +0530

    editedd few commands

--- docker-build.sh
+++ docker-build.sh
@@ -1,6 +1,7 @@
 
 #docker run --runtime=nvidia --rm -ti -v "${PWD}:/app" tensorflow/tensorflow:latest-gpu-jupyter
 docker stop $(docker ps -a -q)
-#docker image prune
+docker rm $(docker ps -a -q)
+#docker image prune -a
 docker image build -t tf_gpu_custom .
 docker run --runtime=nvidia --detach --name lsh_gpu_environment tf_gpu_custom
--- dockerfile
+++ dockerfile
@@ -1,6 +1,7 @@
 FROM  tensorflow/tensorflow:latest-gpu-jupyter
 
 RUN apt-get update && apt-get install -y python3-pip
+
 RUN pip3 install numpy
 RUN pip3 install pandas
 RUN pip3 install sklearn
